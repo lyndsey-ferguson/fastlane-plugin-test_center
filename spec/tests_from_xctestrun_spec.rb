8dd6ef9153c983d246319977c716c9d2a0b56246
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2018-11-30 10:56:57 -0500

    issue-86 address issue when enable-testability is turned off

--- spec/tests_from_xctestrun_spec.rb
+++ spec/tests_from_xctestrun_spec.rb
@@ -59,4 +59,27 @@ describe Fastlane::Actions::TestsFromXctestrunAction do
       ]
     )
   end
+
+  it 'displays an error if no tests found in a xctestrun' do
+    allow(File).to receive(:exist?).and_call_original
+    allow(File).to receive(:exist?).with('path/to/fake.xctestrun').and_return(true)
+    allow(File).to receive(:read).with('path/to/fake.xctestrun').and_return(File.read('./spec/fixtures/fake.xctestrun'))
+    allow(XCTestList)
+      .to receive(:tests)
+      .with('path/to/Debug-iphonesimulator/AtomicBoy.app/PlugIns/AtomicBoyTests.xctest')
+      .and_return([])
+    allow(XCTestList)
+      .to receive(:tests)
+      .with('path/to/Debug-iphonesimulator/AtomicBoyUITests-Runner.app/PlugIns/AtomicBoyUITests.xctest')
+      .and_return([])
+    fastfile = "lane :test do
+      tests_from_xctestrun(
+        xctestrun: 'path/to/fake.xctestrun'
+      )
+    end"
+    expect(FastlaneCore::UI).to receive(:error).with(/^No tests found.*/).twice
+    expect(FastlaneCore::UI).to receive(:important).with(/^Is the Build Setting, `ENABLE_TESTABILITY` enabled.*/).twice
+    tests = Fastlane::FastFile.new.parse(fastfile).runner.execute(:test)
+    expect(tests).to eq({'AtomicBoyTests' => [], 'AtomicBoyUITests' => [] })
+  end
 end
