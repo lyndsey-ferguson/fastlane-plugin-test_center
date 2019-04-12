be870c7dedf766c211f1689be8f796ada27610ec
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2019-04-12 10:12:40 -0400

    new action

---
+++ lib/fastlane/plugin/test_center/actions/quit_core_simulator_service.rb
@@ -0,0 +1,36 @@
+module Fastlane
+  module Actions
+    class QuitCoreSimulatorServiceAction < Action
+      def self.run(params)
+        launchctl_list_count = 0
+        while Actions.sh('launchctl list | grep com.apple.CoreSimulator.CoreSimulatorService || true', log: false) != ''
+          UI.crash!('Unable to quit com.apple.CoreSimulator.CoreSimulatorService after 10 tries') if (launchctl_list_count += 1) > 10
+          commands << Actions.sh('launchctl remove com.apple.CoreSimulator.CoreSimulatorService &> /dev/null || true', log: false)
+          UI.verbose('Waiting for com.apple.CoreSimulator.CoreSimulatorService to quit')
+          sleep(0.25)
+        end
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        "Quits the ccom.apple.CoreSimulator.CoreSimulatorService."
+      end
+
+      def self.details
+        "Sometimes the com.apple.CoreSimulator.CoreSimulatorService can hang. " \
+        "Use this action to force-quit it so that it can restart fresh."
+      end
+
+      def self.authors
+        ["lyndsey-ferguson/@lyndseydf"]
+      end
+
+      def self.is_supported?(platform)
+        platform == :ios
+      end
+    end
+  end
+end
