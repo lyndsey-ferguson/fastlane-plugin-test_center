03585fbf65a93a26279a8e7a4b222441dc440c1e
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2019-05-09 13:40:28 -0400

    use Scan Runner directly

--- lib/fastlane/plugin/test_center/helper/multi_scan_manager/retrying_scan.rb
+++ lib/fastlane/plugin/test_center/helper/multi_scan_manager/retrying_scan.rb
@@ -12,12 +12,20 @@ module TestCenter
           try_count = @options[:try_count] || 1
           begin
             valid_scan_keys = Fastlane::Actions::ScanAction.available_options.map(&:key)
-            config = FastlaneCore::Configuration.create(
-              Fastlane::Actions::ScanAction.available_options,
-              @scan_options.select { |k,v| valid_scan_keys.include?(k) }
-            )
-
-            Fastlane::Actions::ScanAction.run(config)
+            scan_options = @options.select { |k,v| valid_scan_keys.include?(k) }
+            scan_options.each do |k,v|
+              Scan.config.set(k,v)
+            end
+            # TODO: Investigate the following error:
+            """
+            2019-05-09 13:32:40.707 xcodebuild[78535:1944070] [MT] DVTAssertions: Warning in /Library/Caches/com.apple.xbs/Sources/IDEFrameworks_Fall2018/IDEFrameworks-14460.46/IDEFoundation/ProjectModel/ActionRecords/IDESchemeActionTestAttachment.m:186
+            Details:  Error writing attachment data to file /Users/lyndsey.ferguson/Library/Developer/Xcode/DerivedData/AtomicBoy-flqqvvvzbouqymbyffgdbtjoiufr/Logs/Test/Test-Transient Testing-2019.05.09_13-32-08--0400.xcresult/1_Test/Attachments/Screenshot_E0AE7940-E7F4-4CA8-BB2B-8822D730D10F.jpg: Error Domain=NSCocoaErrorDomain Code=4 \"The folder “Screenshot_E0AE7940-E7F4-4CA8-BB2B-8822D730D10F.jpg” doesn’t exist.\" UserInfo={NSFilePath=/Users/lyndsey.ferguson/Library/Developer/Xcode/DerivedData/AtomicBoy-flqqvvvzbouqymbyffgdbtjoiufr/Logs/Test/Test-Transient Testing-2019.05.09_13-32-08--0400.xcresult/1_Test/Attachments/Screenshot_E0AE7940-E7F4-4CA8-BB2B-8822D730D10F.jpg, NSUserStringVariant=Folder, NSUnderlyingError=0x7fa6ef34ef90 {Error Domain=NSPOSIXErrorDomain Code=2 \"No such file or directory\"}}
+            Object:   <IDESchemeActionTestAttachment: 0x7fa6ef22d270>
+            Method:   -_savePayload:
+            Thread:   <NSThread: 0x7fa6ea516110>{number = 1, name = main}
+            Please file a bug at https://bugreport.apple.com with this warning message and any useful information you can provide.
+            """
+            Scan::Runner.new.run
             @retrying_scan_helper.after_testrun
             true
           rescue FastlaneCore::Interface::FastlaneTestFailure => e
