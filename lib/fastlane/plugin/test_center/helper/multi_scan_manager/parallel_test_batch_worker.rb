07cdf732f7337983c899cc1fbb8830c8e340cd0b
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2019-06-05 08:52:02 -0400

    add parallelTestBatchWorker run

--- lib/fastlane/plugin/test_center/helper/multi_scan_manager/parallel_test_batch_worker.rb
+++ lib/fastlane/plugin/test_center/helper/multi_scan_manager/parallel_test_batch_worker.rb
@@ -2,6 +2,11 @@ module TestCenter
   module Helper
     module MultiScanManager
       class ParallelTestBatchWorker < TestBatchWorker
+        def run(run_options)
+          Process.fork do
+            RetryingScan.run(@options.merge(run_options))
+          end
+        end
       end
     end
   end
--- spec/multi_scan_manager/parallel_test_batch_worker_spec.rb
+++ spec/multi_scan_manager/parallel_test_batch_worker_spec.rb
@@ -1,4 +1,14 @@
 module TestCenter::Helper::MultiScanManager
   describe 'ParallelTestBatchWorker', refactor_retrying_scan:true do
+    describe '#run' do
+      it 'calls the parent class\'s #run method in a fork block' do
+        worker = ParallelTestBatchWorker.new({})
+        expect(Process).to receive(:fork) do |&block|
+          expect(RetryingScan).to receive(:run)
+          block.call()
+        end
+        worker.run({})
+      end
+    end
   end
 end
