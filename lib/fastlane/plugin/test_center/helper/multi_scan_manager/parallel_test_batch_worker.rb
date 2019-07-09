e5b53120d29a7e60c87c8732368f3e925efa477a
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2019-07-09 17:48:28 -0400

    address more comments

--- lib/fastlane/plugin/test_center/helper/multi_scan_manager/parallel_test_batch_worker.rb
+++ lib/fastlane/plugin/test_center/helper/multi_scan_manager/parallel_test_batch_worker.rb
@@ -1,6 +1,10 @@
 module TestCenter
   module Helper
     module MultiScanManager
+      require 'colorize'
+
+      colors = String.color_samples - %i[white black light_green]
+
       class ParallelTestBatchWorker < TestBatchWorker
 
         attr_reader :pid
@@ -8,6 +12,8 @@ module TestCenter
         def initialize(options)
           super(options)
           @pipe_endpoint = nil
+          @color = colors.sample
+          colors = colors - [@color]
         end
 
         def state=(new_state)
@@ -15,9 +21,14 @@ module TestCenter
         end
 
         def process_results
+          # This is performed in the Parent process
           @pid = nil
+          worker_prefix = "[worker #{@options[:batch_index] + 1}]"
+          unless Helper.colors_disabled?
+            worker_prefix.colorize(@color)
+          end
           File.foreach(@log_filepath) do |line|
-            puts "[worker #{@options[:batch_index] + 1}] #{line}"
+            puts "#{worker_prefix} #{line}"
           end
           state = :ready_to_work
           @options[:test_batch_results] << (@reader.gets == 'true')
@@ -44,6 +55,11 @@ module TestCenter
         end
 
         def open_interprocess_communication
+          # This is performed in the Parent process in preparation to setup
+          # the STDOUT and STDOUT for printing messages from the Child process
+          # to a file. This is done so that when multiple processes write
+          # messages, they will not be written to the console in a broken
+          # interlaced manner.
           @reader, @writer = IO.pipe
           @log_filepath = File.join(
             Dir.mktmpdir,
@@ -52,7 +68,8 @@ module TestCenter
         end
 
         def reroute_stdout_to_logfile
-          @reader.close # we are now in the subprocess
+          @reader.close # we are now in the subprocess. Write all stdout to the
+          # log file to prevent interlaced messages
           @logfile = File.open(@log_filepath, 'w')
           @logfile.sync = true
           $stdout.reopen(@logfile)
@@ -60,6 +77,7 @@ module TestCenter
         end
 
         def handle_child_process_results(tests_passed)
+          # as suggested by the method name, this is done in the Child process
           @logfile.close
           @writer.puts tests_passed.to_s
           @writer.close
@@ -72,5 +90,3 @@ module TestCenter
     end
   end
 end
-
-
