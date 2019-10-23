92400657ed57a9561330e0a71ce076b9d10b63bf
jdoshi <jdoshi@indeed.com>
2019-10-23 08:20:35 -0700

    Added shellscape for result bundle files

--- lib/fastlane/plugin/test_center/actions/collate_test_result_bundles.rb
+++ lib/fastlane/plugin/test_center/actions/collate_test_result_bundles.rb
@@ -59,8 +59,10 @@ module Fastlane
           tmp_collated_bundlepath = File.join(dir, File.basename(collated_bundlepath))
           xcresulttool_cmd = 'xcrun xcresulttool merge '
           xcresulttool_cmd += tmp_xcresult_bundlepaths.map(&:shellescape).join(' ')
-          xcresulttool_cmd += " --output-path #{tmp_collated_bundlepath}"
+          xcresulttool_cmd += " --output-path #{tmp_collated_bundlepath.shellescape}"
+          UI.message("%%%%%%%%%%%%%%%%%%%%%%%%%%%")
           UI.message(xcresulttool_cmd)
+          UI.message("%%%%%%%%%%%%%%%%%%%%%%%%%%%")
           sh(xcresulttool_cmd)
           FileUtils.safe_unlink(tmp_xcresult_bundlepaths)
           FileUtils.rm_rf(collated_bundlepath)
