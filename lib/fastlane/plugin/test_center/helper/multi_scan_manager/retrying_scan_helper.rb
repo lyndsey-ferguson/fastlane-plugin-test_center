cb8fdc9c0dfcec0f92f0edfc42bf97fc6ded5510
Benedek Kozma <benedek.kozma@tresorit.com>
2022-10-14 10:15:05 +0200

    pass proper option instead of xcargs

--- lib/fastlane/plugin/test_center/helper/multi_scan_manager/retrying_scan_helper.rb
+++ lib/fastlane/plugin/test_center/helper/multi_scan_manager/retrying_scan_helper.rb
@@ -179,7 +179,7 @@ module TestCenter
             }
           )
           if @reportnamer.includes_xcresult?
-            retrying_scan_options[:xcargs] += "-resultBundlePath '#{File.join(output_directory, @reportnamer.xcresult_last_bundlename)}' "
+            retrying_scan_options[:result_bundle_path] = File.join(output_directory, @reportnamer.xcresult_last_bundlename)
           end
 
           @options.select { |k,v| valid_scan_keys.include?(k) }
